6b90a5136f5c837f9e406eb675470de000e2b108
Gurkirpal Singh <gurkirpal204@gmail.com>
2017-05-25 04:21:16 +0530

    st/omx_tizonia: Add AVC decoder

---
+++ src/gallium/state_trackers/omx_tizonia/h264dprc.h
@@ -0,0 +1,8 @@
+#ifndef H264DPRC_H
+#define H264DPRC_H
+
+void * h264d_prc_class_init (void * ap_tos, void * ap_hdl);
+void * h264d_prc_init (void * ap_tos, void * ap_hdl);
+
+#endif                          /* H264DPRC_H */
+
