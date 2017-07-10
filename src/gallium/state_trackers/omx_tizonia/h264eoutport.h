3d66862ceea83cdbb182a09ebfe02d19486d2a69
Gurkirpal Singh <gurkirpal204@gmail.com>
2017-07-10 11:01:58 +0530

    omx_tizonia: Add H.264 encoder
    v2: [Julien] name for 'factory_new' and 'tiz_comp_init' requires to be the same

---
+++ src/gallium/state_trackers/omx_tizonia/h264eoutport.h
@@ -0,0 +1,7 @@
+#ifndef H264EOUTPORT_H
+#define H264EOUTPORT_H
+
+void * h264e_outport_class_init (void * ap_tos, void * ap_hdl);
+void * h264e_outport_init (void * ap_tos, void * ap_hdl);
+
+#endif /* H264EOUTPORT_H */
